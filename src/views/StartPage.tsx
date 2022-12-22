6c4bc7be9d61a5fb9a57ccd069e10b8d228888a8
HXL1111 <2521556749@qq.com>
2022-12-22 20:05:51 +0800

    完成 Tabs 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,6 +1,6 @@
-import { NavBarLayout } from '@/Layouts/NavBarLayout'
-import { Icon } from '@/shared/Icon'
-import { defineComponent, PropType } from 'vue'
+import { NavBarLayout } from '@/layouts/NavBarLayout'
+import { Tabs, Tab } from '@/shared/Tabs'
+import { defineComponent, PropType, ref } from 'vue'
 import s from './StartPage.module.scss'
 export const StartPage = defineComponent({
   props: {
@@ -9,10 +9,22 @@ export const StartPage = defineComponent({
     },
   },
   setup: (props, context) => {
+    const refKind = ref('本月')
     return () => (
       <div class={s.wrapper}>
         <NavBarLayout iconName="menu" title="i 记账">
-          {{ default: () => <div>main</div> }}
+          {{
+            default: () => (
+              <>
+                <Tabs v-model:selected={refKind.value}>
+                  <Tab name="本月">列表1</Tab>
+                  <Tab name="上个月">列表2</Tab>
+                  <Tab name="今年">列表3</Tab>
+                  <Tab name="自定义时间">列表4</Tab>
+                </Tabs>
+              </>
+            ),
+          }}
         </NavBarLayout>
       </div>
     )
