8ac2bf640bf55afe85758403e66c710b8c9e9e10
HXL1111 <2521556749@qq.com>
2023-01-04 10:35:51 +0800

    加入登录页表单验证

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -2,7 +2,8 @@ import { NavBarLayout } from '@/layouts/NavBarLayout'
 import { Button } from '@/shared/Button'
 import { Form, FormItem } from '@/shared/Form'
 import { Icon } from '@/shared/Icon'
-import { defineComponent, PropType } from 'vue'
+import { validate } from '@/shared/validate'
+import { defineComponent, PropType, reactive } from 'vue'
 import s from './SignInPage.module.scss'
 export const SignInPage = defineComponent({
   props: {
@@ -12,6 +13,34 @@ export const SignInPage = defineComponent({
   },
 
   setup: (props, context) => {
+    const formData = reactive({
+      email: '',
+      code: '',
+    })
+    const errors = reactive({
+      email: [],
+      code: [],
+    })
+    const onSubmit = (e: Event) => {
+      e.preventDefault()
+      Object.assign(errors, {
+        email: [],
+        code: [],
+      })
+      Object.assign(
+        errors,
+        validate(formData, [
+          { key: 'email', type: 'required', message: '必填' },
+          {
+            key: 'email',
+            type: 'pattern',
+            regex: /.+@.+/,
+            message: '必须是邮箱地址',
+          },
+          { key: 'code', type: 'required', message: '必填' },
+        ])
+      )
+    }
     const onClickSendValidationCode = () => {
       console.log('1')
     }
@@ -22,17 +51,19 @@ export const SignInPage = defineComponent({
             <Icon name="wallet" class={s.icon} />
             <h1>i 记账</h1>
           </div>
-          <Form>
+          <Form onSubmit={onSubmit}>
             <FormItem
               label="邮箱地址"
               type="text"
               placeholder="请输入邮箱，然后点击发送验证码"
+              error={errors.email?.[0]}
             />
             <FormItem
               onClick={onClickSendValidationCode}
               label="验证码"
               type="validationCode"
               placeholder="6 位数字"
+              error={errors.code?.[0]}
             />
             <div class={s.button_wrapper}>
               <Button class={s.button}>登录</Button>
