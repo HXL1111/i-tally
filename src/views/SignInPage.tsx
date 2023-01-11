f747b48249111dd7f564e090c38abf6567334b21
HXL1111 <2521556749@qq.com>
2023-01-11 15:33:24 +0800

    删除测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -49,11 +49,8 @@ export const SignInPage = defineComponent({
       )
       if (!hasError(errors)) {
         const response = await http
-          .post<{ jwt: string }>('session', formData, {
-            params: { _mock: 'session' },
-          })
+          .post<{ jwt: string }>('session', formData)
           .catch(onSubmitError)
-        console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
         // const returnTo = localStorage.getItem('returnTo')
