c73ae9081d4e83f52ff8ef8839ddd5ad92e31e71
HXL1111 <2521556749@qq.com>
2023-01-17 14:29:14 +0800

    升级vite3

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -23,12 +23,15 @@ export const ItemSummary = defineComponent({
       if (!props.startDate || !props.endDate) {
         return
       }
-      const response = await http.get<Resources<Item>>('/items', {
-        happen_after: props.startDate,
-        happen_before: props.endDate,
-        page: page.value + 1,
-        _mock: 'itemIndex',
-      })
+      const response = await http.get<Resources<Item>>(
+        '/items',
+        {
+          happen_after: props.startDate,
+          happen_before: props.endDate,
+          page: page.value + 1,
+        },
+        { _mock: 'itemIndex', _autoLoading: true }
+      )
       const { resources, pager } = response.data
       items.value?.push(...resources)
       hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,38 @@
+import { routes } from './config/routes';
 import { createApp } from 'vue'
 import { App } from './App'
-import { routes } from './config/routes'
-import { createRouter } from 'vue-router';
+import { createRouter } from 'vue-router'
 import { history } from './shared/history';
+import '@svgstore';
+import { fetchMe, mePromise } from './shared/me';
 import '@/assets/stylesheet/reset.scss'
 import '@/assets/stylesheet/vars.scss'
-import '@svgstore'
 import 'vant/lib/index.css';
-import { fetchMe, mePromise } from './shared/me';
-
-const router = createRouter({
-  history,
-  routes,
-})
+const router = createRouter({ history, routes })
 
 fetchMe()
 
-router.beforeEach(async (to, from) => {
-  if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path.startsWith('/item')) {
-    return true
-  } else {
-    return mePromise!.then(() => true, () => {
-      return 'sign_in?return_to=' + to.path
-    })
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith',
+}
+
+router.beforeEach((to, from) => {
+  for (const key in whiteList) {
+    const value = whiteList[key]
+    if (value === 'exact' && to.path === key) {
+      return true
+    }
+    if (value === 'startsWith' && to.path.startsWith(key)) {
+      return true
+    }
   }
+  return mePromise!.then(
+    () => true,
+    () => '/sign_in?return_to=' + to.path
+  )
 })
 
 const app = createApp(App)
