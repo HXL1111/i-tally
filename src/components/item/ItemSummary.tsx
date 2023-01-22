1f561b099b2d4ed07ada13f2cb14aed7b8aeb85a
HXL1111 <2521556749@qq.com>
2023-01-22 09:53:43 +0800

    封装useItemStore

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -3,7 +3,8 @@ import { DateTime } from '@/shared/DateTime'
 import { http } from '@/shared/Http'
 import { Icon } from '@/shared/Icon'
 import { Money } from '@/shared/Money'
-import { defineComponent, onMounted, PropType, reactive, ref, watch } from 'vue'
+import { useItemStore } from '@/stores/useItemStore'
+import { defineComponent, onMounted, PropType, reactive, watch } from 'vue'
 import { RouterLink } from 'vue-router'
 import s from './ItemSummary.module.scss'
 export const ItemSummary = defineComponent({
@@ -16,40 +17,21 @@ export const ItemSummary = defineComponent({
     },
   },
   setup: (props, context) => {
-    const items = ref<Item[]>([])
-    const hasMore = ref(false)
-    const page = ref(0)
-    const fetchItems = async () => {
-      if (!props.startDate || !props.endDate) {
-        return
-      }
-      const response = await http.get<Resources<Item>>(
-        '/items',
-        {
-          happen_after: props.startDate,
-          happen_before: props.endDate,
-          page: page.value + 1,
-        },
-        { _mock: 'itemIndex', _autoLoading: true }
-      )
-      const { resources, pager } = response.data
-      items.value?.push(...resources)
-      items.value.reverse()
-      hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
-      page.value += 1
+    if (!props.startDate || !props.endDate) {
+      return () => <div>请先选择时间范围</div>
     }
+    const itemStore = useItemStore(['items', props.startDate, props.endDate])
+
+    onMounted(() => {
+      itemStore.fetchItems(props.startDate, props.endDate)
+    })
     watch(
       () => [props.startDate, props.endDate],
       () => {
-        items.value = []
-        hasMore.value = false
-        page.value = 0
-        fetchItems()
+        itemStore.reset()
+        itemStore.fetchItems()
       }
     )
-    onMounted(() => {
-      fetchItems()
-    })
 
     const itemsBalance = reactive({
       expenses: 0,
@@ -65,7 +47,7 @@ export const ItemSummary = defineComponent({
         {
           happen_after: props.startDate,
           happen_before: props.endDate,
-          page: page.value + 1,
+          page: itemStore.page + 1,
         },
         { _mock: 'itemIndexBalance' }
       )
@@ -85,7 +67,7 @@ export const ItemSummary = defineComponent({
     )
     return () => (
       <div class={s.itemSummary}>
-        {items.value && items.value.length > 0 ? (
+        {itemStore.items && itemStore.items.length > 0 ? (
           <>
             <ol class={s.total}>
               <li class={s.expense}>
@@ -108,7 +90,7 @@ export const ItemSummary = defineComponent({
               </li>
             </ol>
             <ol class={s.recordList}>
-              {items.value.map((item) => (
+              {itemStore.items.map((item) => (
                 <li>
                   <div class={s.left}>
                     <div class={s.logo}>
@@ -128,7 +110,11 @@ export const ItemSummary = defineComponent({
               ))}
             </ol>
             <div class={s.more}>
-              {hasMore.value ? <Button onClick={fetchItems}>加载更多</Button> : <span>没有更多</span>}
+              {itemStore.hasMore ? (
+                <Button onClick={() => itemStore.fetchItems(props.startDate, props.endDate)}>加载更多</Button>
+              ) : (
+                <span>没有更多</span>
+              )}
             </div>
           </>
         ) : (
