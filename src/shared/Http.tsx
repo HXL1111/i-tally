2188905d7b4096b67fecd9936c665c1742b24c0e
HXL1111 <2521556749@qq.com>
2023-01-31 14:46:09 +0800

    解决 bug

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -94,7 +94,7 @@ function isDev() {
   return true
 }
 
-export const http = new Http(isDev() ? 'api/v1' : 'http://121.196.236.94:3000/api/v1')
+export const http = new Http(isDev() ? '/api/v1' : 'http://121.196.236.94:3000/api/v1')
 
 http.instance.interceptors.request.use((config) => {
   const jwt = localStorage.getItem('jwt')
